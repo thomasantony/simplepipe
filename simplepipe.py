f18d576fcc9e8ad9d4811d2f910e0bdee04d0e54
Thomas Antony <tantony@purdue.edu>
2016-07-24 17:01:09 -0400

    Implemented hooks using functools partials

--- simplepipe.py
+++ simplepipe.py
@@ -18,7 +18,9 @@ class Workflow:
         """
         Creates a point in the workflow where hook functions can be added
         """
-        self.tasks.append({'hook': name})
+        self.tasks.append({'task': functools.partial(self.run_hook, name),
+                            'inputs': '*',
+                            'outputs': '*'})
         return self
 
     def add_hook(self, name, function):
@@ -46,17 +48,26 @@ class Workflow:
     def __call__(self, data_in={}):
         data = copy.deepcopy(data_in)
         for task in self.tasks:
-            if 'hook' in task:
-                result = self.run_hook(task['hook'], data)
-                data.update(result)
+            # Input is full workspace for input type '*'
+            if task['inputs'] == '*' or task['inputs']==['*']:
+                inputs = [data]
             else:
                 inputs = [data[key] for key in task['inputs']]
-                results = task['task'](*inputs)
-                if not hasattr(results, '__iter__') or isinstance(results, str):
-                    results = [results]
-                if len(results) != len(task['outputs']):
-                    raise RuntimeError('Number of return values does not match number of outputs')
 
+            results = task['task'](*inputs)
+
+            if len(task['outputs']) == 1:
+                results = [results]
+
+            if len(results) != len(task['outputs']):
+                raise RuntimeError('Number of return values does not match number of outputs')
+
+            if task['outputs'] == '*' or task['outputs'] == ['*']:
+                try:
+                    data.update(results[0])
+                except TypeError as e:
+                    raise TypeError('Result should be a dict for output type *')
+            else:
                 data.update(zip(task['outputs'],results))
         return data
 
