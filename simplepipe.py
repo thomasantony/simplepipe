25845c067efe5877da13f42f0c2081ead05243b9
Thomas Antony <tantony@purdue.edu>
2016-07-24 16:22:51 -0400

    Added basic hook functionality

--- simplepipe.py
+++ simplepipe.py
@@ -3,23 +3,65 @@ Created on 24 July 2016
 @author: Thomas Antony
 """
 import copy
+import functools
 
-class Pipeline:
+class Workflow:
     def __init__(self, task_list=[]):
         self.tasks = task_list
+        self.hooks = {}
 
     def add_task(self, task, inputs=[], outputs=[]):
         self.tasks.append({'task':task, 'inputs':inputs, 'outputs':outputs})
+        return self
 
-    def run(self, data_in={}):
+    def add_hook_event(self, name):
+        """
+        Creates a point in the workflow where hook functions can be added
+        """
+        self.tasks.append({'hook': name})
+        return self
+
+    def add_hook(self, name, function):
+        """
+        Adds a function to be called for hook of a given name
+
+        The function gets entire workspace as input and does not return anything
+        """
+        if not callable(function):
+            return ValueError('Hook function should be callable')
+        if name not in self.hooks:
+            self.hooks[name] = []
+        self.hooks[name].append(function)
+
+    def run_hook(self, name, data_in):
+        if name not in self.hooks:
+            raise KeyError('Hook '+name+' not found')
+
+        data = copy.deepcopy(data_in)
+        for hook_fn in self.hooks[name]:
+            # Hook functions may mutate the data and returns nothing
+            hook_fn(data)
+        return data
+
+    def __call__(self, data_in={}):
         data = copy.deepcopy(data_in)
         for task in self.tasks:
-            inputs = [data_in[key] for key in task['inputs']]
-            results = task['task'](*inputs)
-            if not hasattr(results, '__iter__') or isinstance(results, str):
-                results = [results]
-            if len(results) != len(task['outputs']):
-                raise RuntimeError('Number of return values does not match number of outputs')
+            if 'hook' in task:
+                result = self.run_hook(task['hook'], data)
+                data.update(result)
+            else:
+                inputs = [data[key] for key in task['inputs']]
+                results = task['task'](*inputs)
+                if not hasattr(results, '__iter__') or isinstance(results, str):
+                    results = [results]
+                if len(results) != len(task['outputs']):
+                    raise RuntimeError('Number of return values does not match number of outputs')
 
-            data.update(zip(task['outputs'],results))
+                data.update(zip(task['outputs'],results))
         return data
+
+    def __len__(self):
+        """
+        Returns number of tasks in workflow
+        """
+        return len(self.tasks)
--- test_simplepipe.py
+++ test_simplepipe.py
@@ -1,19 +1,38 @@
 import simplepipe
+def sum(a, b):
+    return a+b
+
+
+
 def test_simplepipe():
     def return_one():
         return 1
 
-    p = simplepipe.Pipeline()
+    p = simplepipe.Workflow()
     p.add_task(return_one, inputs=[], outputs=['a'])
-    output = p.run()
+    output = p()
     assert(output == {'a': 1})
 
+
+    p = simplepipe.Workflow()
     data_in = {'a':1, 'b':2}
     data_out = {'a':1, 'b':2, 'c':3}
-    def sum(a, b):
-        return a+b
-
-    p = simplepipe.Pipeline()
     p.add_task(sum, inputs=['a','b'], outputs=['c'])
-    output = p.run(data_in)
+    output = p(data_in)
     assert(output == data_out)
+
+def test_hooks():
+    p = simplepipe.Workflow()
+    data_in = {'a':1, 'b':2}
+    data_out = {'a':1, 'b':2, 'c':3}
+    p.add_task(sum, inputs=['a','b'], outputs=['c'])
+    p.add_hook_event('after_sum')
+
+    def after_sum_1(data):
+        data['c'] = 10
+
+    p.add_task(lambda x: 2*x, inputs=['c'], outputs=['d'])
+
+    p.add_hook('after_sum', after_sum_1)
+    output = p(data_in)
+    assert(output == {'a': 1, 'b': 2, 'c': 10, 'd': 20})
