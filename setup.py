63abe12be830c145c222526970381cabeeda5eea
Thomas Antony <tantony@purdue.edu>
2016-07-24 21:20:30 -0400

    Made run_task a pure function

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "simplepipe",
+    version = "0.0.1",
+    author = "Thomas Antony",
+    author_email = "tantony.purdue@gmail.com",
+    description = ("A simple functional pipelining library for Python."),
+    license = "MIT",
+    keywords = "pipline, functional, functional programming",
+    url = "http://packages.python.org/simplepipe",
+    packages=['simplepipe', 'tests'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
--- simplepipe.py
+++ simplepipe.py
@@ -17,6 +17,9 @@ class Workflow(object):
     def add_hook_event(self, name):
         """
         Creates a point in the workflow where hook functions can be added
+
+        Implemented as a special type of task that takes full workspace as
+        input and returns a modified workspace
         """
         self.tasks.append({'task': functools.partial(self.run_hook, name),
                             'inputs': '*',
@@ -28,12 +31,17 @@ class Workflow(object):
         Adds a function to be called for hook of a given name
 
         The function gets entire workspace as input and does not return anything
+
+        Example:
+        def hook_fcn(workspace):
+            pass
         """
         if not callable(function):
             return ValueError('Hook function should be callable')
         if name not in self.hooks:
             self.hooks[name] = []
         self.hooks[name].append(function)
+        return self
 
     def run_hook(self, name, workspace):
         """
@@ -42,37 +50,61 @@ class Workflow(object):
         if name not in self.hooks:
             raise KeyError('Hook '+name+' not found')
 
-        data = copy.deepcopy(workspace)
-        for hook_fn in self.hooks[name]:
+        data = copy.copy(workspace)
+        for hook_listener in self.hooks[name]:
             # Hook functions may mutate the data and returns nothing
-            hook_fn(data)
+            hook_listener(data)
         return data
 
     def __call__(self, workspace={}):
-        data = copy.deepcopy(workspace)
+        """
+        Executes all the queued tasks in order and returns
+        new workspace with results
+        """
+        result = workspace
         for task in self.tasks:
-            # Input is full workspace for input type '*'
-            if task['inputs'] == '*' or task['inputs']==['*']:
-                inputs = [data]
-            else:
-                inputs = [data[key] for key in task['inputs']]
-
-            results = task['task'](*inputs)
-
-            if len(task['outputs']) == 1:
-                results = [results]
-
-            if len(results) != len(task['outputs']):
-                raise RuntimeError('Number of return values does not match number of outputs')
-
-            if task['outputs'] == '*' or task['outputs'] == ['*']:
-                try:
-                    data.update(results[0])
-                except TypeError as e:
-                    raise TypeError('Result should be a dict for output type *')
-            else:
-                data.update(zip(task['outputs'],results))
-        return data
+            result = run_task(task, result)
+        return result
 
     def __repr__(self):
         return '<%s with %d tasks>' % (self.__class__.__name__, len(self.tasks))
+
+def run_task(task, workspace):
+    """
+    Runs the task and updates the workspace with results
+
+    task : dict describing task:
+
+    Examples:
+    {'task': task_func, 'inputs': ['a','b'], 'outputs': 'c'}
+    {'task': task_func, 'inputs': '*', 'outputs': '*'}
+
+    Returns a new workspace with results
+    """
+    data = copy.copy(workspace)
+    # Input is full workspace for input type '*'
+    if task['inputs'] == '*' or task['inputs']==['*']:
+        inputs = [data]
+    else:
+        inputs = [data[key] for key in task['inputs']]
+
+    if not callable(task['task']):
+        raise ValueError('Task function must be a callable object')
+
+    results = task['task'](*inputs)
+
+    if len(task['outputs']) == 1:
+        results = [results]
+
+    if len(results) != len(task['outputs']):
+        raise RuntimeError('Number of return values does not match number of outputs')
+
+    if task['outputs'] == '*' or task['outputs'] == ['*']:
+        try:
+            data.update(results[0])
+        except TypeError as e:
+            raise TypeError('Result should be a dict for output type *')
+    else:
+        data.update(zip(task['outputs'],results))
+
+    return data
